2ce295650f92fda4f95da3eaa9f30b9dd6bfd7b1
Gaston Doval <gdoval@interno.interdotnet.com.ar>
2018-09-07 18:11:26 -0300

    flata algo

---
+++ Programacion/TpParte1/Fabrica.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Fabrica
+ *
+ * @author gdoval
+ */
+class Fabrica {
+    //put your code here
+    
+    private $_cantidadMaxima;
+    private $_empleados;
+    private $_razonSocial;
+    
+    public function __construct($razonSocial)
+    {
+        $this->_razonSocial = $razonSocial;
+        $this->_cantidadMaxima = 5;
+        $this->_empleados = array();
+    }
+    
+    public function AgregarEmpleado($empleado)
+    {
+        $cant = count($this->_empleados);
+        if ($cant < $this->_cantidadMaxima)
+        {
+            array_push($this->_empleados, $empleado);
+        }else
+            return -1;
+    }
+    
+    public function ToString()
+    {
+        /* @var $value Empleado */ //Asi se declara en la IDE que la variable $value es de tipo Empleado. Un casteo, vamos.
+        foreach($this->_empleados as &$value)
+        {
+            echo $value->ToString();
+            echo "<br/>";
+        }
+    }
+}
