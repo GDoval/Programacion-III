f0fb6231efefe9c7e5176b72494ded034a83defa
GDoval <gedoval@gmail.com>
2018-11-07 21:24:14 -0300

    clase10 bootstrap

---
+++ Laboratorio/Clase10_Bootstrap/administrador.js
@@ -0,0 +1,30 @@
+/// <reference path="libs/jquery/index.d.ts" />
+$(document).ready(function () {
+    $("#btnAceptar").on("click", EjemploBootstrap);
+});
+function EjemploBootstrap() {
+    if ($("#btnAceptar").hasClass("btn-info")) {
+        $("#btnAceptar").removeClass("btn-info");
+        $("#btnAceptar").addClass("btn-success");
+    }
+    else if ($("#btnAceptar").hasClass("btn-success")) {
+        $("#btnAceptar").removeClass("btn-success");
+        $("#btnAceptar").addClass("btn-warning");
+    }
+    else if ($("#btnAceptar").hasClass("btn-warning")) {
+        $("#btnAceptar").removeClass("btn-warning");
+        $("#btnAceptar").addClass("btn-danger");
+    }
+    else if ($("#btnAceptar").hasClass("btn-danger")) {
+        $("#btnAceptar").removeClass("btn-danger");
+        $("#btnAceptar").addClass("btn-default");
+    }
+    else if ($("#btnAceptar").hasClass("btn-default")) {
+        $("#btnAceptar").removeClass("btn-default");
+        $("#btnAceptar").addClass("btn-info");
+    }
+    //var cont:number=0;
+    //let boton=(<HTMLButtonElement>document.getElementById("btnAceptar"));
+    //boton.cla
+    //if()
+}
